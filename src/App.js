8b1ca7947520f40345a39ce049d540a35d673c25
Max <maxrogerssepulveda@gmail.com>
2020-09-23 15:41:49 -0300

    Unnecessary files deleted

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
 import React from "react";
-import "./App.css";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 // Components
 import Links from "./components/Links";
-import LinkForm from "./components/LinkForm";
 
 function App() {
   return (
